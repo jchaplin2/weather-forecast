e997b8997ac0f175f5e1f49e8ce41751c798af75
Your Name <jchaplin@gmail.com>
2020-02-07 08:27:48 -0600

    refactor SearchBar to separate component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import {createUseStyles} from 'react-jss'
 
-const useStyles = createUseStyles({
-    '.search-input' : {
-        width: '50%',
-        margin: '15px auto 0px auto'
-    }
-});
+import SearchBar from './SearchBar';
 
 const App = () => {
-    const classes = useStyles();
-
-    return (
-        <div className="ui grid">
-            <div className={"ui action input " + classes[".search-input"]}>
-                <input type="text" placeholder="Enter Location" />
-                <button className="ui icon button">
-                    <i className="search icon"></i>
-                </button>
-            </div>            
-        </div>
-    ); 
+    return <SearchBar/>;
 };
 
 export default App;
