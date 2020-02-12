448b0ef61113a5052a866731f046ade0eedf6247
Your Name <jchaplin@gmail.com>
2020-02-12 17:40:25 -0600

    make request to weather api

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import {createUseStyles} from 'react-jss'
 
-const useStyles = createUseStyles({
-    '.search-input' : {
+const API_KEY = '7650d1c9ec87898004f754e19e5b71ae';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&q=`;
+
+const setStyles = createUseStyles({
+    'form':{
+        display: 'inline-block',
         width: '50%',
         margin: '15px auto 0px auto'
+    },
+    '.search-input' : {
+        width: '100%'
     }
 });
 
 const SearchBar = () => {
-    const classes = useStyles();
+    const classes = setStyles();
+    const [location, setLocation] = useState('');
+
+    const fetchWeatherData = async () => {
+        const url = ROOT_URL + location;
+        const response = await axios.get(url);
+        console.log(response);
+    };
+
+    const handleLocationChange = event => {
+        setLocation(event.target.value);
+    };
+
+    const handleClick = () => {
+        fetchWeatherData();
+    };
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetchWeatherData();
+    }
 
     return (
         <div className="ui grid">
-            <div className={"ui action input " + classes[".search-input"]}>
-                <input type="text" placeholder="Enter Location" />
-                <button className="ui icon button">
-                    <i className="search icon"></i>
-                </button>
-            </div>            
+            <form className={classes["form"]} onSubmit={handleSubmit}>
+                <div className={"ui action input " + classes[".search-input"]}>
+                        <input type="text" onChange={handleLocationChange} placeholder="Enter Location" />
+                        <button className="ui icon button" onClick={handleClick}>
+                            <i className="search icon" ></i>
+                        </button>
+                </div>
+            </form> 
         </div>
     );
+
 }
 
 export default SearchBar;
