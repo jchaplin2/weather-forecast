9ffa449b34bb99f4f6f26b5ca279cc81e3386d38
Your Name <jchaplin@gmail.com>
2020-02-25 17:55:22 -0600

    refactor/clean up

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,10 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const WEATHER_API_KEY = 'ebdf7693b8212976a3017b9638488edc';
-const LOCATION_API_KEY = 'AIzaSyBgH6ZLz9Pyrq2emfnGeFppvMWgL9KYuVM';
+const WEATHER_API_KEY = "ebdf7693b8212976a3017b9638488edc";
+const LOCATION_API_KEY = "AIzaSyBgH6ZLz9Pyrq2emfnGeFppvMWgL9KYuVM";
 
-
-const DARK_SKY_PROXY = 'https://cors-anywhere.herokuapp.com/';
+const DARK_SKY_PROXY = "https://cors-anywhere.herokuapp.com/";
 //work around for dak sky CORS issue with darksky.
 
 const ROOT_WEATHER_URL = `https://api.darksky.net/forecast/${WEATHER_API_KEY}/`;
@@ -14,31 +13,34 @@ const ROOT_LOCATION_URL = `https://maps.googleapis.com/maps/api/geocode/json?key
 // const ROOT_WEATHER_URL = 'http://localhost:4000/weather?q=';
 // const ROOT_LOCATION_URL = 'http://localhost:4000/location?components=';
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_WEATHER = "FETCH_WEATHER";
 
-export async function fetchWeatherData (location) {
-    const {latitude, longitude} = await fetchLocationData(location);
-    const url = DARK_SKY_PROXY + ROOT_WEATHER_URL + latitude + "," + longitude;
-    const {data} = await axios.get(url);
+export async function fetchWeatherData(location) {
+  const { latitude, longitude } = await fetchLocationData(location);
+  const url = DARK_SKY_PROXY + ROOT_WEATHER_URL + latitude + "," + longitude;
+  const { data } = await axios.get(url);
 
-    console.log(data);
-	return {
-		type: FETCH_WEATHER,
-		payload: data
-	};
-};
+  return {
+    type: FETCH_WEATHER,
+    payload: data
+  };
+}
 
-async function fetchLocationData (location) {
-    const locationArray = location.split(",");
-    const [town, state] = locationArray;
-    const url = ROOT_LOCATION_URL + "locality:"+town+"|administrative_area_level_1:"+state;
-    const response = await axios.get(url);
+async function fetchLocationData(location) {
+  const locationArray = location.split(",");
+  const [town, state] = locationArray;
+  const url =
+    ROOT_LOCATION_URL +
+    "locality:" +
+    town +
+    "|administrative_area_level_1:" +
+    state;
+  const response = await axios.get(url);
 
-    const{ lat, lng } = response.data.results[0].geometry.location;
-    console.log(lat, lng);
+  const { lat, lng } = response.data.results[0].geometry.location;
 
-	return {
-        latitude : lat,
-        longitude: lng
-	};
-};
+  return {
+    latitude: lat,
+    longitude: lng
+  };
+}
--- src/components/WeatherForecastList.jsx
+++ src/components/WeatherForecastList.jsx
@@ -1,97 +1,121 @@
-import React from 'react';
-import { createUseStyles } from 'react-jss';
-import { connect } from 'react-redux';
+import React from "react";
+import { createUseStyles } from "react-jss";
+import { connect } from "react-redux";
 
 const setStyles = createUseStyles({
-    'rounded-border':{
-        borderRadius: "3px",
-        border: "solid 1px black",
-        margin: "2px"
-    },
-    'yellow': {
-        color: "#F0E68C"
-    },
-    'blue':{
-        color:'#67C8FF'
-    },
-    'gray':{
-        color:'#708090'
-    }
+  "rounded-border": {
+    borderRadius: "3px",
+    border: "solid 1px black",
+    margin: "2px"
+  },
+  "margin-bottom": {
+    marginBottom: "1em"
+  },
+  "margin-top": {
+    marginTop: ".5em"
+  },
+  "large-icon": {
+    fontSize: "90px"
+  }
 });
 
-const WeatherForecastList = (state) => {
-        const {weather} = state;
-        const classes = setStyles();
+let darkSkyToFontAwesomeIconMap = {
+  "clear-day": "fa fa-sun yellow",
+  "clear-night": "fa fa-moon-stars blue",
+  rain: "fa fa-cloud-rain gray",
+  snow: "fa fa-snowflake blue",
+  sleet: "fa fa-cloud-showers-heavy blue",
+  wind: "fa fa-wind blue",
+  fog: "fa fa-smog gray",
+  cloudy: "fa fa-cloud blue",
+  "partly-cloudy-day": "fa fa-cloud-sun blue",
+  "partly-cloudy-night": "fa fa-moon-cloud blue",
+  hail: "fa fa-cloud-meatball gray",
+  thunderstorm: "fa fa-bolt yellow",
+  tornado: "fa fa-wind blue"
+};
 
-        let darkSkyToFontAwesomeIconMap = {
-            "clear-day" : "fa fa-sun " + classes['yellow'],
-            "clear-night": "fa fa-moon-stars "+ classes['blue'],
-            "rain": "fa fa-cloud-rain " + classes['gray'],
-            "snow": "fa fa-snowflake "+ classes['blue'],
-            "sleet": "fa fa-cloud-showers-heavy",
-            "wind": "fa fa-wind" + classes['blue'],
-            "fog": "fa fa-smog " + classes['gray'],
-            "cloudy": "fa fa-cloud "+ classes['blue'],
-            "partly-cloudy-day": "fa fa-cloud-sun "+ classes['blue'],
-            "partly-cloudy-night": "fa fa-moon-cloud "+ classes['blue'],
-            "hail": "fa fa-cloud-meatball " + classes['gray'],
-            "thunderstorm": "fa fa-bolt "+ classes['yellow'],
-            "tornado": "fa fa-wind "+ classes['blue']
-        }
+const WeatherForecastList = state => {
+  const { weather } = state;
 
-        if(!weather) {
-            return <div className="row"></div>;
-        }
+  if (!weather) {
+    return <div className="row"></div>;
+  }
 
-        const {data} = weather.daily;
-        const dailyForecastData = data.slice(0,5);
-        //doing a 5 day forecast, so only need 5 days.
+  const classes = setStyles();
+  const { data } = weather.daily;
+  const dailyForecastData = data.slice(0, 5);
+  //doing a 5 day forecast, so take first 5 days.
 
-        const getIconClass = (weatherType) => {
-            let iconClass = darkSkyToFontAwesomeIconMap[weatherType];
-            //TODO: default icon.
-            if(!iconClass)
-                iconClass = "fa fa-wind";
-            return iconClass;
-        };
+  const getIconClass = weatherType => {
+    let iconClass = darkSkyToFontAwesomeIconMap[weatherType];
 
-        const renderWeatherList = (dailyForecastData) => {
-            return dailyForecastData.map((dailyForecast, index) => {
-                const classString = getIconClass(dailyForecast.icon);
-                const humidityPercent = Math.round(dailyForecast.humidity * 100);
-                const precipitationPercent = dailyForecast.precipProbability * 100;
-                const precipitationMax = dailyForecast.precipIntensityMax.toFixed(2);
+    //if weather type not found show warning icon.
+    if (!iconClass) iconClass = "fa fa-exclamation-triangle";
 
-                const {sunriseTime, sunsetTime} = dailyForecast;
-                const sunriseDate = new Date(sunriseTime * 1000);
-                const sunsetDate = new Date(sunsetTime * 1000);
+    return iconClass;
+  };
 
-                return (
-                    //TODO refactor styles.
-                    <div key={index} className={"three wide column " + classes['rounded-border']} >
-                        <div style={{"fontSize":"90px","marginBottom":"25px","marginTop":"25px"}}>
-                            <i className={classString}></i>
-                        </div>
-                        <div>Hi / Low:</div>
-                        <div style={{"marginBottom":"25px"}} >{dailyForecast.temperatureHigh} / {dailyForecast.temperatureLow}</div>
-                        <div style={{"marginBottom":"25px"}}>
-                            {dailyForecast.summary}
-                        </div>
+  const renderWeatherList = dailyForecastData => {
+    return dailyForecastData.map((dailyForecast, index) => {
+      const {
+        sunriseTime,
+        sunsetTime,
+        icon,
+        humidity,
+        precipProbability,
+        precipIntensityMax
+      } = dailyForecast;
 
-                        <div style={{"marginBottom":"25px"}} >Humidity: { humidityPercent } %</div>
-                        <div style={{"marginBottom":"25px"}} >Precipitation: {precipitationPercent} % chance  ({precipitationMax} in.)</div>
-                        <div style={{"marginBottom":"25px"}} >Wind: {dailyForecast.windSpeed} mph </div>
-                        <div style={{"marginBottom":"25px"}} > Sunrise: {sunriseDate.getHours()}:{sunriseDate.getMinutes()}am / Sunset: {sunsetDate.getHours() % 12}:{sunsetDate.getMinutes()}pm </div>
-                    </div>
-                );
-            });
-        };
+      const classString = getIconClass(icon);
+      const humidityPercent = Math.round(humidity * 100);
+      const precipitationPercent = Math.round(precipProbability * 100);
+      const precipitationMax = precipIntensityMax.toFixed(2);
+      const sunriseDate = new Date(sunriseTime * 1000);
+      const sunsetDate = new Date(sunsetTime * 1000);
 
-        return <div className="row"> { renderWeatherList(dailyForecastData) } </div>;
-}
+      return (
+        <div
+          key={index}
+          className={"three wide column " + classes["rounded-border"]}
+        >
+          <div
+            className={`${classes["margin-top"]} ${classes["margin-bottom"]} ${classes["large-icon"]}`}
+          >
+            <i className={classString}></i>
+          </div>
+          <div>Hi / Low:</div>
+          <div className={classes["margin-bottom"]}>
+            {dailyForecast.temperatureHigh} &deg; F /{" "}
+            {dailyForecast.temperatureLow} &deg; F
+          </div>
+          <div className={classes["margin-bottom"]}>
+            {dailyForecast.summary}
+          </div>
+          <div className={classes["margin-bottom"]}>
+            Humidity: {humidityPercent} %
+          </div>
+          <div className={classes["margin-bottom"]}>
+            Precipitation: {precipitationPercent} % chance ({precipitationMax}{" "}
+            in.)
+          </div>
+          <div className={classes["margin-bottom"]}>
+            Wind: {dailyForecast.windSpeed} mph
+          </div>
+          <div className={classes["margin-bottom"]}>
+            Sunrise: {sunriseDate.getHours()}:{sunriseDate.getMinutes()} am /
+            Sunset: {sunsetDate.getHours() % 12}:{sunsetDate.getMinutes()} pm
+          </div>
+        </div>
+      );
+    });
+  };
 
-function mapStateToProps( state ) {
-    return state.weather;
+  return <div className="row"> {renderWeatherList(dailyForecastData)} </div>;
+};
+
+function mapStateToProps(state) {
+  return state.weather;
 }
 
 export default connect(mapStateToProps)(WeatherForecastList);
