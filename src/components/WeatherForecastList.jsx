63c8210750e86df935f5c8b91fff636a501bd6e1
Your Name <jchaplin@gmail.com>
2020-02-25 12:39:25 -0600

    add daily weather info and styling.

--- src/components/WeatherForecastList.jsx
+++ src/components/WeatherForecastList.jsx
@@ -7,51 +7,81 @@ const setStyles = createUseStyles({
         borderRadius: "3px",
         border: "solid 1px black",
         margin: "2px"
+    },
+    'yellow': {
+        color: "#F0E68C"
+    },
+    'blue':{
+        color:'#67C8FF'
+    },
+    'gray':{
+        color:'#708090'
     }
 });
-
-let darkSkyToFontAwesomeIconMap = {
-    "clear-day" : "fa fa-sun",
-    "clear-night": "fa fa-moon-stars",
-    "rain": "fa fa-cloud-rain",
-    "snow": "fa fa-snowflake",
-    "sleet": "fa fa-cloud-showers-heavy",
-    "wind": "fa fa-wind",
-    "fog": "fa fa-smog",
-    "cloudy": "fa fa-cloud",
-    "partly-cloudy-day": "fa fa-cloud-sun",
-    "partly-cloudy-night": "fa fa-moon-cloud",
-    "hail": "fa fa-cloud-meatball",
-    "thunderstorm": "fa fa-bolt",
-    "tornado": "fa fa-wind"
-}
 
 const WeatherForecastList = (state) => {
         const {weather} = state;
         const classes = setStyles();
-        console.log(weather);
+
+        let darkSkyToFontAwesomeIconMap = {
+            "clear-day" : "fa fa-sun " + classes['yellow'],
+            "clear-night": "fa fa-moon-stars "+ classes['blue'],
+            "rain": "fa fa-cloud-rain " + classes['gray'],
+            "snow": "fa fa-snowflake "+ classes['blue'],
+            "sleet": "fa fa-cloud-showers-heavy",
+            "wind": "fa fa-wind" + classes['blue'],
+            "fog": "fa fa-smog " + classes['gray'],
+            "cloudy": "fa fa-cloud "+ classes['blue'],
+            "partly-cloudy-day": "fa fa-cloud-sun "+ classes['blue'],
+            "partly-cloudy-night": "fa fa-moon-cloud "+ classes['blue'],
+            "hail": "fa fa-cloud-meatball " + classes['gray'],
+            "thunderstorm": "fa fa-bolt "+ classes['yellow'],
+            "tornado": "fa fa-wind "+ classes['blue']
+        }
+
         if(!weather) {
             return <div className="row"></div>;
         }
 
         const {data} = weather.daily;
         const dailyForecastData = data.slice(0,5);
-        //only need 5 days.
+        //doing a 5 day forecast, so only need 5 days.
 
         const getIconClass = (weatherType) => {
             let iconClass = darkSkyToFontAwesomeIconMap[weatherType];
+            //TODO: default icon.
             if(!iconClass)
                 iconClass = "fa fa-wind";
             return iconClass;
         };
 
         const renderWeatherList = (dailyForecastData) => {
-            return dailyForecastData.map(dailyForecast => {
+            return dailyForecastData.map((dailyForecast, index) => {
                 const classString = getIconClass(dailyForecast.icon);
+                const humidityPercent = Math.round(dailyForecast.humidity * 100);
+                const precipitationPercent = dailyForecast.precipProbability * 100;
+                const precipitationMax = dailyForecast.precipIntensityMax.toFixed(2);
+
+                const {sunriseTime, sunsetTime} = dailyForecast;
+                const sunriseDate = new Date(sunriseTime * 1000);
+                const sunsetDate = new Date(sunsetTime * 1000);
+
                 return (
-                    <div className={"three wide column " + classes['rounded-border']} >
-                        <i class={classString}></i>
-                        {dailyForecast.summary}
+                    //TODO refactor styles.
+                    <div key={index} className={"three wide column " + classes['rounded-border']} >
+                        <div style={{"fontSize":"90px","marginBottom":"25px","marginTop":"25px"}}>
+                            <i className={classString}></i>
+                        </div>
+                        <div>Hi / Low:</div>
+                        <div style={{"marginBottom":"25px"}} >{dailyForecast.temperatureHigh} / {dailyForecast.temperatureLow}</div>
+                        <div style={{"marginBottom":"25px"}}>
+                            {dailyForecast.summary}
+                        </div>
+
+                        <div style={{"marginBottom":"25px"}} >Humidity: { humidityPercent } %</div>
+                        <div style={{"marginBottom":"25px"}} >Precipitation: {precipitationPercent} % chance  ({precipitationMax} in.)</div>
+                        <div style={{"marginBottom":"25px"}} >Wind: {dailyForecast.windSpeed} mph </div>
+                        <div style={{"marginBottom":"25px"}} > Sunrise: {sunriseDate.getHours()}:{sunriseDate.getMinutes()}am / Sunset: {sunsetDate.getHours() % 12}:{sunsetDate.getMinutes()}pm </div>
                     </div>
                 );
             });
