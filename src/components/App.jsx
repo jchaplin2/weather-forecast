8351e17fdf6be07f06ddabb95c6e6587e374c5a9
Your Name <jchaplin@gmail.com>
2020-03-08 11:48:00 -0500

    Add component for today's forecast view

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 
 import SearchBar from "./SearchBar";
 import WeatherForecastList from "./WeatherForecastList";
+import WeatherForecastDaily from "./WeatherForecastDaily";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <main className="ui center aligned grid">
-      <SearchBar />
-      <WeatherForecastList />
-    </main>
+    <Router>
+      <main className="ui center aligned grid">
+        <SearchBar />
+        <Switch>
+          <Route path="/" exact component={WeatherForecastList} />
+          <Route path="/today" exact component={WeatherForecastDaily} />
+        </Switch>
+      </main>
+    </Router>
   );
 };
 
---
+++ src/components/WeatherForecastDaily.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const WeatherForecastDaily = state => {
+  return <div>Todays Forecast</div>;
+};
+
+export default WeatherForecastDaily;
